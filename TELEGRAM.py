12c6535321f22e1302b8cf9e62d154e492932a0d
MasoodManzoorAhmed <115372328+MasoodManzoorAhmed@users.noreply.github.com>
2022-10-09 01:04:38 +0530

    Add files via upload

---
+++ TELEGRAM.py
@@ -0,0 +1,12 @@
+import telepot
+
+
+##bot = telepot.Bot('5704374867:AAFY-ubG7ytDiQ-PW84oAtuoME0By3socUQ')
+##bot.sendMessage('1056518640', str('Emergency at http://www.google.com/maps/?q={},{}'.format('13.0379','77.6190')))
+##bot.sendPhoto('1056518640', photo=open('frame.png', 'rb'))
+
+
+
+bot = telepot.Bot('5704374867:AAFY-ubG7ytDiQ-PW84oAtuoME0By3socUQ')
+bot.sendMessage('1214514766', str('Emergency at http://www.google.com/maps/?q={},{}'.format('13.0379','77.6190')))
+bot.sendPhoto('1214514766', photo=open('frame.png', 'rb'))
---
+++ aa.py
@@ -0,0 +1,3 @@
+long=22.5
+lat=77.9
+print('Emergency at http://www.google.com/maps/?q={},{}'.format(long,lat))
